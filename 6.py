a00dfabfe67988f2a49fd13df8e60bd1ac5f00df
skeb0 <143435323+skeb0@users.noreply.github.com>
2024-07-26 20:22:09 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,20 @@
+!pip install pgmpy 
+
+import pandas as pd
+data = pd.read_csv('datasetheart.csv', names=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'RESULT']) 
+
+from pgmpy.models import BayesianModel 
+from pgmpy.estimators import BDeuScore, BayesianEstimator 
+from pgmpy.inference import VariableElimination 
+print(data.head(5)) 
+print(data.tail(5)) 
+from pgmpy.models import BayesianModel 
+from pgmpy.estimators import MaximumLikelihoodEstimator 
+
+model = BayesianModel([("A","B"),("B","C"),("C","D"),("D","RESULT")]) 
+model.fit(data,estimator=MaximumLikelihoodEstimator) 
+
+from pgmpy.inference import VariableElimination 
+infer = VariableElimination(model) 
+q = infer.query(variables=['RESULT'],evidence={"C":"2"}) 
+print(q) 
